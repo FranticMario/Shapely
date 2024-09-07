04475ddce0e990de7c8f3d205b8126d27fc23b70
Andrej Dortmann <andrej.dortmann25@gmail.com>
2024-09-07 13:42:25 +0200

    feat: add burger menu with js

---
+++ assets/script/customers.js
@@ -0,0 +1,17 @@
+const arrCustomers = {
+    person__first: {
+        kommentare: "Integer ut rutrum nulla, sit amet fringilla nisi. Nam nisl velit, vehicula at nibh sed, porttitor sollicitudin ante. Sed justo augue, vestibulum ut efficitur vitae, euismod et justo. Mauris sed mattis ante. Ut ligula lectus, consequat vitae neque ac, gravida hendrerit ante.",
+        image: "../img/",
+        name: "John Doe"
+    },
+    person__second: {
+        kommentare: "",
+        image: "",
+        name: ","
+    },
+    person__three: {
+        kommentare: "",
+        image: "",
+        name: ","
+    }
+}
