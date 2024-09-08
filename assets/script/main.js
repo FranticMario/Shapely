72ed8c50b89b09d790134faf132193956da375d8
Andrej Dortmann <andrej.dortmann25@gmail.com>
2024-09-08 12:28:53 +0200

    feat: add swipe cutomers

--- assets/script/main.js
+++ assets/script/main.js
@@ -36,18 +36,19 @@ const swipe = () => {
     const swipeIcon = document.querySelectorAll(".swipe__icon");
 
     const showCostumers = (e) => {
-        let target = e.target
-        console.log(target)
+
         costumersContainer.forEach(item=> {
             if(item.classList.contains("costumers__show")) {
               return  item.classList.toggle("costumers__show");
             } else {
-               return item.classList.toggle("costumers__show")
+
+               return item.classList.toggle("costumers__show" )
             }
         })
     }
 
     swipeIcon.forEach(item =>  item.addEventListener("click", (e) => {
+
         showCostumers(e)
     } ))
   
